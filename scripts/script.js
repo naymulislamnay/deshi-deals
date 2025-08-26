474a2fde809893b11fe20ea339fee93d9b6c4ec7
naymulislamnay <naymulislamnay@gmail.com>
2025-08-26 16:13:35 +0600

    coupon code feature added

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 
 const cartButtons = document.getElementsByClassName('cart-btn');
+let totalPrice = 0;
+let discountPrice = 0;
+let finalPrice = 0;
+let couponApplied = false;
+let discountPercentage = 0;
 
 
 
@@ -8,6 +13,8 @@ for (let cartButton of cartButtons) {
         const productImage = cartButton.parentNode.childNodes[1].childNodes[1].src;
         const productName = cartButton.parentNode.childNodes[5].innerText;
         const productPrice = cartButton.parentNode.childNodes[7].childNodes[0].innerText;
+
+        const priceValue = Number(productPrice);
 
         // parent node
         const cartParentDiv = document.getElementById('cart-parent-div');
@@ -22,16 +29,78 @@ for (let cartButton of cartButtons) {
             <h3 class="text-[20px] font-bold">${productName}</h3>
             <p class="text-[20px] font-bold opacity-50">${productPrice} TK</p>
         </div>
-        </div>`
+        </div>`;
 
         cartParentDiv.appendChild(newCartItem);
 
-        // const totalPrice = 
 
+        // update total Price
+        totalPrice += priceValue;
 
+        updatePrices();
     })
 }
 
 
+// apply Coupon
 
+document.getElementById('coupon-btn').addEventListener('click', function () {
+    const validCouponCode = document.getElementById('coupon-code').innerText;
+    const couponCodeInput = document.getElementById('coupon-code-input').value;
+
+    if (validCouponCode === couponCodeInput) {
+        discountPercentage = Number(document.getElementById('discount-percentage').innerText);
+        couponApplied = true;
+
+        updatePrices();
+    }
+
+    else {
+        alert('Invalid Coupon Code')
+    }
+})
+
+
+// document.getElementById('coupon-btn').addEventListener('click', function () {
+//     const validCouponCode = document.getElementById('coupon-code').innerText;
+//     const couponCodeInput = document.getElementById('coupon-code-input').value;
+//     const discountPercentage = Number(document.getElementById('discount-percentage').innerText);
+
+//     if (validCouponCode === couponCodeInput) {
+//         discountPrice = totalPrice * (discountPercentage / 100);
+//         finalPrice = totalPrice - discountPrice;
+
+//         document.getElementById('discount').innerText = discountPrice.toFixed(2);
+
+
+//         document.getElementById('final-price').innerText = finalPrice.toFixed(2);
+//     }
+
+//     else {
+//         alert('Invalid Coupon Code')
+//     }
+
+//     console.log(couponCodeInput)
+//     console.log(discountPrice)
+// })
+
+
+// function to update the price
+
+
+function updatePrices() {
+    if (couponApplied) {
+        discountPrice = totalPrice * (discountPercentage / 100);
+        finalPrice = totalPrice - discountPrice;
+    }
+
+    else {
+        discountPrice = 0;
+        finalPrice = totalPrice
+    }
+
+    document.getElementById('total-price').innerText = totalPrice.toFixed(2);
+    document.getElementById('total-discount').innerText = discountPrice.toFixed(2);
+    document.getElementById('final-price').innerText = finalPrice.toFixed(2);
+}
 
